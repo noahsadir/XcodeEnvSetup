43bb3915033dcf2b15ab5f521989744930e6a765
Noah Sadir <development.noahsadir@gmail.com>
2024-09-03 18:07:08 -0400

    Add Xcode project files

---
+++ XcodeEnvSetup/XcodeEnvSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeEnvSetupApp.swift
+//  XcodeEnvSetup
+//
+//  Created by Noah Sadir on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeEnvSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
